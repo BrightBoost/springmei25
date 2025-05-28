26505db6f6bd67ae752b0a3daaf1cd82cc32832d
maaikevanputten <maaike.vanputten@brightboost.nl>
2025-05-28 11:41:26 +0200

    security

--- src/main/java/com/example/demo/LoggingAspect.java
+++ src/main/java/com/example/demo/LoggingAspect.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -16,9 +18,19 @@ public class LoggingAspect {
         System.out.println("Before: " + joinPoint.getSignature().getName());
     }
 
+    @Around("execution(* com.example.demo.*.*(..))")
+    public void logExecutionTimeEndpoint(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.currentTimeMillis();
+        pjp.proceed();
+        long end = System.currentTimeMillis();
+        System.out.println(pjp.getSignature().getName() + " took " + (end - start) + "ms");
+    }
+
     @After("@annotation(com.example.demo.Loggable)")
     public void logSomethingAfter(JoinPoint joinPoint) {
         System.out.println("Annotation loggable in action! On: " + joinPoint.getSignature().getName());
     }
+
+
 
 }
---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.aspectj.lang.annotation.Before;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    @Order(1)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/example").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
